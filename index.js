2aae28d076430f144aefd973459c2b26eb4eefb3
Sannu <smahat.official@gmail.com>
2018-08-21 20:03:07 +0300

    Handling Get and Post Requests

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
+const Joi = require('joi');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const skills = [
+    { id: 1, name: 'React' },
+    { id: 2, name: 'Redux' },
+    { id: 3, name: 'JavaScript' },
+    { id: 4, name: 'HTML5' },
+    { id: 5, name: 'CSS3/SASS/SCSS/LESS' },
+    { id: 6, name: 'Node.js' },
+    { id: 7, name: 'Unit Testing' },
+    { id: 8, name: 'Styled Components' }
+
+];
 app.get('/', (request, response) => {
-    response.send('Hello World!!!!');
+    response.send('Hello Worlds!!!!!!');
 });
 
 app.get('/sandhya/skills', (request, response) => {
-    response.send(['React', 'Redux', 'JavaScript', 'HTML5', 'CSS3/SASS/SCSS/LESS', 'Node.js', 'Unit Testing', 'MongoDB']);
+    response.send(skills);
 });
 
-app.get('sandhya/skills/:id', (request, response) => {
-    response.send(request.params.id);
-})
+app.post('/sandhya/skills', (request, response) => {
+    if (!request.body.name) {
+        // 400 Bad Request
+        response.status(400).send('Name is required');
+        return;
+    }
+    else if (request.body.name.length < 3) {
+        response.status(400).send('Name should be longer than 3 characters.');
+        return;
+    }
+
+
+    const skill = {
+        id: skills.length + 1,
+        name: request.body.name
+    }
+    skills.push(skill);
+    response.send(skill);
+});
+
+app.get('/sandhya/skills/:id', (request, response) => {
+    const skill = skills.find(skill => skill.id === parseInt(request.params.id));
+    if (!skill) response.status(404).send('The skill with the given ID was not found.');
+    response.send(skill);
+});
 
 const port = process.env.PORT || 3001;
-app.listen(3001, () => console.log(`Listening to Port ${port}`));
+app.listen(3001, () => console.log(`Listening to Port ${port}...`));
 
