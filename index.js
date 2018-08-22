1ffcc4e9d7a0f2a1b8e49f876caebe0bb5548144
Sannu <smahat.official@gmail.com>
2018-08-22 17:59:01 +0300

    Handling HTTP Put Requests

--- index.js
+++ index.js
@@ -19,20 +19,19 @@ app.get('/', (request, response) => {
     response.send('Hello Worlds!!!!!!');
 });
 
+//Retrieve skills
 app.get('/sandhya/skills', (request, response) => {
     response.send(skills);
 });
 
+//Create new skill
 app.post('/sandhya/skills', (request, response) => {
-    const schema = {
-        name: Joi.string().min(3).required()
-    };
 
-    const result = Joi.validate(request.body, schema);
-
-    if (result.error) {
+    const { error } = validateSkill(request.body); //Object destructuring. Same as result.error
+    //If invalid, return 400 Bad Request
+    if (error) {
         // 400 Bad Request
-        response.status(400).send(result.error.details[0].message);
+        response.status(400).send(error.details[0].message);
         return;
     }
 
@@ -44,11 +43,51 @@ app.post('/sandhya/skills', (request, response) => {
     response.send(skill);
 });
 
+//Update a skill
+app.put('/sandhya/skills/:id', (req, res) => {
+
+    //Look for the course
+    const skill = skills.find(skill => skill.id === parseInt(req.params.id));
+
+    //If not exist, return 404 Resource not found
+    if (!skill) res.status(404).send('The skill with the given ID was not found.');
+
+    //If exists, validate
+    const { error } = validateSkill(req.body); //Object destructuring. Same as result.error
+    //If invalid, return 400 Bad Request
+    if (error) {
+        // 400 Bad Request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    // Else Update the Skill
+    skill.name = req.body.name;
+
+    //Return the updated course
+    res.send(skill);
+})
+
+
+
 app.get('/sandhya/skills/:id', (request, response) => {
+
+    //Look for the skill
     const skill = skills.find(skill => skill.id === parseInt(request.params.id));
+
+    //If not exist, return 404 Resource not found
     if (!skill) response.status(404).send('The skill with the given ID was not found.');
+
+    // Else Return the skill
     response.send(skill);
 });
+
+function validateSkill(skill) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(skill, schema);
+}
 
 const port = process.env.PORT || 3001;
 app.listen(3001, () => console.log(`Listening to Port ${port}...`));
